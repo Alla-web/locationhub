f7e1bfe0fc28761f03f312f3fa51187c89d72a51
Helena Mitiaieva <h0636641547@gmail.com>
2026-04-04 17:56:30 +0200

    update feedbacks endpoint for home reviews

--- src/validations/feedbackValidation.js
+++ src/validations/feedbackValidation.js
@@ -4,13 +4,13 @@ export const getFeedbacksSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(1).max(100).default(10),
-    locationId: Joi.string().hex().length(24).required(),
+    locationId: Joi.string().hex().length(24),
   }),
 };
 
 export const createFeedbacksSchema = {
   [Segments.BODY]: Joi.object({
-    locationId: Joi.string().hex().length(24).required(),
+    locationId: Joi.string().hex().length(24).optional(),
     text: Joi.string().trim().min(1).required(),
     rate: Joi.number().min(1).max(5).required(),
   }),
