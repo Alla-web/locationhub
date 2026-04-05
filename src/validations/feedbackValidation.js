ed6e284034b2f50cf3f04aeac04c2a436c3b6dd8
Helena Mitiaieva <h0636641547@gmail.com>
2026-04-05 15:40:53 +0200

    delete in getFeedbacksSchema() in locationId .requied()

--- src/validations/feedbackValidation.js
+++ src/validations/feedbackValidation.js
@@ -4,7 +4,7 @@ export const getFeedbacksSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(1).max(100).default(10),
-    locationId: Joi.string().hex().length(24).required(),
+    locationId: Joi.string().hex().length(24),
   }),
 };
 
