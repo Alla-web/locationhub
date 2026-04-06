b8b83d37a73a105dec90078c83680253e97cbd13
BMM <m.bazyk@gmail.com>
2026-04-06 22:18:16 +0300

    Merge branch 'main' into User_Update

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,14 +1,8 @@
 import { Region } from '../models/region.js';
 import { LocationType } from '../models/locationType.js';
-import { Location } from '../models/location.js';
 
 export const getCategoriesWithRegions = async (req, res) => {
-  const usedRegionIds = await Location.distinct('regionId');
-
-  //з фільтрацією тільки використовуваних в локаціях регіонів
-  const regions = await Region.find({
-    _id: { $in: usedRegionIds },
-  })
+  const regions = await Region.find()
     .sort({ region: 1 })
     .collation({ locale: 'uk', strength: 1 });
 
@@ -16,12 +10,7 @@ export const getCategoriesWithRegions = async (req, res) => {
 };
 
 export const getCategoriesWithLocationTypes = async (req, res) => {
-  //з використанням тільки використовуваних в локаціях типів локацій
-  const usedLocationTypeIds = await Location.distinct('locationTypeId');
-
-  const locationTypes = await LocationType.find({
-    _id: { $in: usedLocationTypeIds },
-  })
+  const locationTypes = await LocationType.find()
     .sort({ type: 1 })
     .collation({ locale: 'uk', strength: 1 });
 
