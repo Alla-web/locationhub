f5146ab6331c75193858df3716a48e38c1ced43e
Alla-Nazarenko <alla.nazarenko2022@gmail.com>
2026-04-13 15:06:57 +0200

    upload photo file errors handling added to the arror middleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,32 @@
 import createHttpError from 'http-errors';
+import multer from 'multer';
 
 export const errorHandler = (err, req, res, next) => {
   console.error('Error Middleware:', err);
+
+  //помилки щодо відправки файлу фото
+  if (err instanceof multer.MulterError) {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+      return res
+        .status(413)
+        .json({ message: 'Файл завеликий. Максимальний розмір — 5 MB' });
+    }
+
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  if (
+    err.message ===
+    'Invalid file type. Only JPEG, JPG, PNG, GIF and WEBP are allowed'
+  ) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  //загальні помилки
 
   if (createHttpError.isHttpError(err)) {
     return res
@@ -11,9 +36,9 @@ export const errorHandler = (err, req, res, next) => {
 
   const isProd = process.env.NODE_ENV === 'production';
 
-  res.status(500).json({
+  res.status(err.status || 500).json({
     message: isProd
       ? 'Something went wrong. Please try again later.'
-      : err.message,
+      : err.message || 'Internal server error',
   });
 };
